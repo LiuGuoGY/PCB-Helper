448c7a0e097688952ad22f1d6b60a44cf29759c1
liuguo <liuguogy@gmail.com>
2020-12-10 22:07:19 +0800

    feat: 增加分割线

--- src/pages/access/access.js
+++ src/pages/access/access.js
@@ -19,11 +19,19 @@ import iconTurn from "../../assets/icon-zhuanhuan.png"
 class MenuElement extends React.Component {
     render() {
         return (
-            <div className="menu_element_parent" onClick={()=>{alert("点击了")}}>
-                <img src={iconTurn} alt="none" className="menu_element_icon"></img>
-                <p className="menu_element_title">单位转换</p>
-                <p className="menu_element_subtitle">绘制 PCB 时常用的长度单位转换。</p>
+            <div className="menu_element_parent" onClick={()=>{}}>
+                <img src={this.props.icon} alt="none" className="menu_element_icon"></img>
+                <p className="menu_element_title">{this.props.title}</p>
+                <p className="menu_element_subtitle">{this.props.subtitle}</p>
             </div>
+        );
+    }
+}
+
+class DividingLine extends React.Component {
+    render() {
+        return (
+            <div className="dividing_line"></div>
         );
     }
 }
@@ -34,11 +42,7 @@ class PageAccess extends React.Component {
             <div style={{width: "100%", height: "100%"}}>
                 <div className="page_title">辅助功能</div>
                 <div className="page_access_menus_parent">
-                    <MenuElement></MenuElement>
-                    <MenuElement></MenuElement>
-                    <MenuElement></MenuElement>
-                    <MenuElement></MenuElement>
-                    <MenuElement></MenuElement>
+                    <MenuElement icon={iconTurn} title="单位转换" subtitle="绘制 PCB 时常用的长度单位转换。"></MenuElement>
                 </div>
             </div>
         );
