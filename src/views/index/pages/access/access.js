69a12bcd007c42ea1fdc99b618d09407ad325faa
liuguo <liuguogy@gmail.com>
2021-02-02 21:38:39 +0800

    feat: 辅助功能添加另外4种

--- src/views/index/pages/access/access.js
+++ src/views/index/pages/access/access.js
@@ -4,7 +4,13 @@ import commonStyles from "../common.module.css";
 import styles from "./access.module.css";
 
 //---icon---
-import iconTurn from "../../assets/icon-zhuanhuan.png"
+import iconTurn from "../../assets/icon-zhuanhuan.svg"
+import iconPeibi from "../../assets/icon-peibi.svg"
+import iconSearch from "../../assets/icon-search.svg"
+import iconLine from "../../assets/icon-line.svg"
+import iconPackage from "../../assets/icon-xinpian.svg"
+
+
 
 class MenuElement extends React.Component {
     render() {
@@ -33,7 +39,12 @@ class PageAccess extends React.Component {
                 <div className={commonStyles.page_title}>辅助功能</div>
                 <div className={styles.page_access_menus_parent}>
                     <MenuElement icon={iconTurn} title="单位转换" subtitle="绘制 PCB 时常用的长度单位转换。" onClick={()=>{createWindow()}}></MenuElement>
+                    <MenuElement icon={iconPeibi} title="电阻配比" subtitle="快速进行常用的电阻分压电路的阻值配比。" onClick={()=>{}}></MenuElement>
+                    <MenuElement icon={iconSearch} title="常用值查询" subtitle="快速查询常用的电阻、电容、电感值。" onClick={()=>{}}></MenuElement>
                     <DividingLine></DividingLine>
+                    <MenuElement icon={iconLine} title="线宽计算" subtitle="根据 PCB 的实际参数确定某一电流值下的最小线宽。" onClick={()=>{}}></MenuElement>
+                    <MenuElement icon={iconPackage} title="封装查询" subtitle="快速查询常用的封装、名称及其尺寸。" onClick={()=>{}}></MenuElement>
+                    
                 </div>
             </div>
         );
