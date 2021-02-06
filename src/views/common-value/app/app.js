f8b7fa117fab2323f08ea40de464a4375d4db2f8
liuguo <liuguogy@gmail.com>
2021-02-06 12:14:25 +0800

    feat: 新建常用值查询窗口

---
+++ src/views/common-value/app/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './app.module.css';
+const { remote } = window.require('electron');
+
+class Content extends React.Component {
+
+    render() {
+        return (
+            <div className={styles.contentView}>
+                <div className={styles.contentHeader}>
+                    <p className={styles.content_header_text}>常用值查询</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.mainView}>
+                <Content></Content>
+            </div>
+        );
+    }
+}
+
+export default App;
