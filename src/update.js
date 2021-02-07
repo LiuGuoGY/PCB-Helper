45eda7a72df74cc1d331e7f1d1525879552a0cd0
liuguo <liuguogy@gmail.com>
2021-02-07 23:23:03 +0800

    feat: 去掉更新相关无用代码

--- src/update.js
+++ src/update.js
@@ -1,16 +1,5 @@
-const { dialog } = require('electron');
+// const { dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
-const http = require('http');
-
-let ymlUrl = "";
-
-if(process.platform == 'darwin'){
-    ymlUrl = "https://github.com/LiuGuoGY/PCB-Helper/releases/latest/download/latest-mac.yml";
-} else if(process.platform == 'win32'){
-    ymlUrl = "https://github.com/LiuGuoGY/PCB-Helper/releases/latest/download/latest.yml";
-} else if(process.platform == 'linux'){
-    
-}
 
 let message = {
     error: '检查更新出错',
@@ -19,52 +8,36 @@ let message = {
     updateNotAva: '现在使用的就是最新版本，不用更新',
 };
 
+// autoUpdater.autoDownload = false;
+
 // see  https://www.electron.build/auto-update#events
 autoUpdater.on('error', function (error) {
-    console.log(error)
-    console.log(message.error)
+  console.log(error)
+  console.log(message.error)
 });
 autoUpdater.on('checking-for-update', function () {
-    console.log(message.checking)
+  console.log(message.checking)
 });
 autoUpdater.on('update-available', function (info) {
-    console.log(message.updateAva)
+  console.log(info);
+  console.log(message.updateAva)
+  // dialog.showMessageBox({
+  //   type: "info",
+  //   message: "检测到有新版本！"
+  // })
 });
 autoUpdater.on('update-not-available', function (info) {
-    console.log(message.updateNotAva)
+  console.log(info);
+  console.log(message.updateNotAva)
 });
+autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+
+})
 
 autoUpdater.on('update-downloaded', info => {
   if (true) { //process.env.NODE_ENV === 'production'
-    // 这里先拉取更新信息，在对话框中显示版本的更新内容
-    console.log("update_on!!!");
-    const req = http.request(ymlUrl, req => {
-      let detail = ''
-      req.setEncoding('utf-8')
-      req.on('data', chunk => {
-        detail += chunk.toString()
-      })
-      req.on('end', () => {
-          console.log(detail);
-        dialog.showMessageBox(
-          {
-            icon: __static + '/favicon.png',
-            type: 'info',
-            title: '软件更新',
-            message: `已更新到最新版本（${info.version}）请重启应用。`,
-            detail: detail,
-            buttons: ['确定']
-          },
-          idx => {
-            // 点击确定的时候执行更新
-            if (idx === 0) {
-              autoUpdater.quitAndInstall()
-            }
-          }
-        )
-      })
-    })
-    req.end()
+    console.log("download-finish!!!");
+    autoUpdater.quitAndInstall();
   }
 })
 module.exports = autoUpdater;
