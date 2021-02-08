8409aa9d1ae4283d9a0fbcb4adf7bb2458513d8a
liuguo <liuguogy@gmail.com>
2021-02-08 23:04:51 +0800

    feat: 同一种窗口只能打开一个

--- src/views/index/pages/access/window-common-value.js
+++ src/views/index/pages/access/window-common-value.js
@@ -8,42 +8,46 @@ const url = require('url')
 let commonValueWindow
 
 function createWindow () {
-  //创建浏览器窗口,宽高自定义具体大小你开心就好
-  commonValueWindow = new BrowserWindow({
-    width: 400, 
-    height: 300,
-    minWidth: 400,
-    minHeight: 300,
-    titleBarStyle: 'hidden',
-    fullscreenable: false,
-    resizable: false,
-    webPreferences: {
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      contextIsolation: false,
-    }
-  })
-  // 加载应用----适用于 react 项目
-  // unitConvertWindow.loadURL('http://localhost:5000/unit-convert.html');
-  let startUrl = "";
-  if(remote.process.env.ELECTRON_START_URL) {
-    startUrl = remote.process.env.ELECTRON_START_URL + "/common-value.html";
+  if(commonValueWindow) {
+    commonValueWindow.show();
   } else {
-    startUrl = url.format({
-      pathname: path.join(remote.process.env.REACT_APP_ROOT_DIR, './build/common-value.html'),
-      protocol: 'file:',
-      slashes: true
-    });
-  }
-  commonValueWindow.loadURL(startUrl);
-  
-  // 打开开发者工具，默认不打开
-  // mainWindow.webContents.openDevTools()
+    //创建浏览器窗口,宽高自定义具体大小你开心就好
+    commonValueWindow = new BrowserWindow({
+      width: 400, 
+      height: 300,
+      minWidth: 400,
+      minHeight: 300,
+      titleBarStyle: 'hidden',
+      fullscreenable: false,
+      resizable: false,
+      webPreferences: {
+        enableRemoteModule: true,
+        nodeIntegration: true,
+        contextIsolation: false,
+      }
+    })
+    // 加载应用----适用于 react 项目
+    // unitConvertWindow.loadURL('http://localhost:5000/unit-convert.html');
+    let startUrl = "";
+    if(remote.process.env.ELECTRON_START_URL) {
+      startUrl = remote.process.env.ELECTRON_START_URL + "/common-value.html";
+    } else {
+      startUrl = url.format({
+        pathname: path.join(remote.process.env.REACT_APP_ROOT_DIR, './build/common-value.html'),
+        protocol: 'file:',
+        slashes: true
+      });
+    }
+    commonValueWindow.loadURL(startUrl);
 
-  // 关闭window时触发下列事件.
-  commonValueWindow.on('closed', function () {
-    commonValueWindow = null
-  })
+    // 打开开发者工具，默认不打开
+    // mainWindow.webContents.openDevTools()
+
+    // 关闭window时触发下列事件.
+    commonValueWindow.on('closed', function () {
+      commonValueWindow = null
+    })
+  }
 }
 
 
